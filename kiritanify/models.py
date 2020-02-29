c788fa1534d4b1101e48c2a0112b4b847667726c
mohuton <46195901+mohuton@users.noreply.github.com>
2020-02-29 15:50:54 +0900

    Write draft voice/caption generation method

--- kiritanify/models.py
+++ kiritanify/models.py
@@ -1,18 +1,18 @@
 from typing import Optional
 
-from bpy.types import Context
+from bpy.types import Context, Sequence
 
 from kiritanify.propgroups import KiritanifyCharacterSetting
 from kiritanify.types import KiritanifyScriptSequence, SoundSequence
-from kiritanify.utils import _seq_setting
+from kiritanify.utils import _seq_setting, _global_setting
 
 
 class CharacterScript:
   """
-  Represents character script, which basically holds two sequence; caption image sequence (KiritanifyScriptSequence) 
-  and voice sound sequence. 
-  
-  Expected to be used as one-time data structure. Do not store permanently and do not use cross Operator. 
+  Represents character script, which basically holds two sequence; caption image sequence (KiritanifyScriptSequence)
+  and voice sound sequence.
+
+  Expected to be used as one-time data structure. Do not store permanently and do not use cross Operator.
   """
   chara: KiritanifyCharacterSetting
   seq: KiritanifyScriptSequence
@@ -46,3 +46,55 @@ class CharacterScript:
       voice_seq=voice_seq,
       context=context,
     )
+
+  def maybe_update_voice(self):
+    should_regenerate: bool = _seq_setting(self.seq) \
+      .voice_cache_state.is_changed(
+      global_setting=_global_setting(self.context),
+      chara=self.chara,
+      seq=self.seq,
+    )
+    if should_regenerate:
+      if self.voice_seq is not None:
+        self._remove_sequence(self.voice_seq)
+        self.voice_seq = None
+      # TODO: sequence generate procedure here
+
+      _seq_setting(self.seq).voice_cache_state.update(
+        global_setting=_global_setting(self.context),
+        chara=self.chara,
+        seq=self.seq
+      )
+
+    self._align_sequence(
+      seq=self.voice_seq,
+      channel=self.channel,
+      frame_final_start=self.seq.frame_final_start,
+    )
+
+  def maybe_update_caption(self):
+    should_regenerate: bool = _seq_setting(self.seq) \
+      .caption_cache_state.is_changed(_global_setting(self.context), self.chara, self.seq)
+
+    if should_regenerate:
+      pass
+
+  def _generate_caption(self):
+    pass
+
+  @staticmethod
+  def _align_sequence(
+      seq: Sequence,
+      channel: Optional[int] = None,
+      frame_final_start: Optional[int] = None,
+      frame_final_end: Optional[int] = None,
+  ):
+    if channel is not None and seq.channel != channel:
+      seq.channel = channel
+    if frame_final_start is not None and seq.frame_final_start != frame_final_start:
+      seq.frame_final_start = frame_final_start
+    if frame_final_end is not None and seq.frame_final_end != frame_final_end:
+      seq.frame_final_end = frame_final_end
+
+  def _remove_sequence(self, seq):
+    self.context.scene.sequence_editor.sequences.remove(seq)
--- kiritanify/ops.py
+++ kiritanify/ops.py
@@ -1,1 +1,35 @@
+import bpy
+from typing import Union, Set, Dict
+
+from bpy.types import Context, ImageSequence
+
+import kiritanify.types
+from kiritanify.models import CharacterScript
+from kiritanify.propgroups import KiritanifyCharacterSetting
+from kiritanify.utils import _global_setting
+
+
+class KIRITANIFY_OT_RunKiritanifyForScripts(bpy.types.Operator):
+  bl_idname = "kiritanify.run_kiritanify_for_scripts"
+  bl_label = "Run KiritanifyForScripts"
+
+  def execute(self, context: Context) -> Set[Union[int, str]]:
+    global_setting = _global_setting(context)
+    chara_for_chan: Dict[int, KiritanifyCharacterSetting] = {
+      chara.caption_channel(global_setting): chara
+      for chara in global_setting.characters
+    }
+    for seq in context.selected_sequences:
+      if not isinstance(seq, ImageSequence):
+        continue
+      seq: kiritanify.types.ImageSequence
+      chara = chara_for_chan[seq.channel]
+      if chara is None:
+        continue
+      cs = CharacterScript.create_from(chara, seq, context)
+      cs.maybe_update_voice()
+      cs.maybe_update_caption()
+    return {'FINISHED'}
+
+
 OPS_CLASSES = []
